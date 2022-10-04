b588cc4f090783f296c821fb8ebcb147eedcaf6f
shinji <osinosin060302@gmail.com>
2022-10-04 00:44:38 +0000

    second　commit

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>メッセージ一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    {{-- メッセージ詳細ページへのリンク --}}
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {{-- メッセージ作成ページへのリンク --}}
+
+
+@endsection
